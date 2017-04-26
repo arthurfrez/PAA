08a911b95090e67f6e02d97660f331c1d4f9787e
Arthur F. Rezende <arthurfrezende@gmail.com>
2017-04-26 11:45:19 -0300

    Updates

--- Genetic.hpp
+++ Genetic.hpp
@@ -1,8 +1,22 @@
+/***********************************************************************
+*	Arthur Floresta Rezende
+* Genetic.hpp - Metodo de Algoritmo Genetico
+*
+* Implementacao do metodo de algoritimo genetico para solucao
+* do Problema do Caixeiro Viajante (em ingles: TSP)
+***********************************************************************/
+
+//=====================================================================
+// BIBLIOTECAS
+//=====================================================================
 #include "MyGraph.hpp"
 #include <cstdio>
 
 // REF: http://www.theprojectspot.com/tutorial-post/applying-a-genetic-algorithm-to-the-travelling-salesman-problem/5
 
+//------------------------------------------------------------------------------
+// GeneticAlgorithm: solucao usando algoritmo genetico
+//------------------------------------------------------------------------------
 double GeneticAlgorithm(MyGraph* g, int* &path) {
   return 0.0;
 }
