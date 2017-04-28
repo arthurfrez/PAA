1debe53430aca183f3f1db167b88133a7a4482d8
Arthur F. Rezende <arthurfrezende@gmail.com>
2017-04-28 10:37:16 -0300

    Changes
    Changed the base struct and added some essential GA methods.

--- Genetic.hpp
+++ Genetic.hpp
@@ -16,32 +16,27 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define MAX_CITY 100
+
 // REF: http://www.theprojectspot.com/tutorial-post/applying-a-genetic-algorithm-to-the-travelling-salesman-problem/5
+// REF: https://www.vivaolinux.com.br/script/Um-algoritmo-genetico-para-o-TSP-(Travel-Salesman-Problem)
 
 int* generate_random_path(int size); //foward declaration
 
-struct Population {
-  int pos;
-  int qnt_p;
-  double fittest_weight;
-  int** tours;
-
-  void get_fittest() {
-
-  }
-
-  Population(int size, int p_size) {
-    qnt_p = size;
-    tours = new int*[qnt_p];
-    for(int i = 0; i < qnt_p; i++) tours[i] = generate_random_path(p_size);
-    get_fittest();
-  }
-
-  ~Population() {
-    for(int i = 0; i < qnt_p; i++) delete tours[i];
-    delete tours;
-  }
+//=====================================================================
+// STRUCT DO INDIVIDUO
+//=====================================================================
+struct individual {
+  int p_size; // tamanho do caminho
+  int path[MAX_CITY]; // caminho
+  double finess; // aptdao do individuo
+  bool state; // estado atual (modificado ou nao na populacao atual)
 };
+
+//------------------------------------------------------------------------------
+// random_to_one: gera um numero pseudo aleatorio entre 0 e 1
+//------------------------------------------------------------------------------
+double random_to_one() { return rand() / (RAND_MAX + 1.0); }
 
 //------------------------------------------------------------------------------
 // generate_random_path: gera um caminho aleatorio
@@ -51,7 +46,6 @@ int* generate_random_path(int size) {
   std::vector<int> tmp(size);
 
   for(int i = 0; i < size; i++) tmp[i] = i; // gerando array sequencial
-  srand(time(0)); // mudando a seed inicial para gerar aleatoriedade
   std::random_shuffle(tmp.begin()+1, tmp.end()); //embaralhando
 
   // copiando vetor para array
@@ -62,9 +56,56 @@ int* generate_random_path(int size) {
 }
 
 //------------------------------------------------------------------------------
+// crossover: mescla os parentes
+//------------------------------------------------------------------------------
+individual crossover(individual parent1, individual parent2) {
+  individual child;
+  int init_pos = (rand() % (parent1.p_size-1)) + 1;
+  int fin_pos = (rand() % (parent1.p_size-1)) + 1;
+
+  if(init_pos > fin_pos) {
+    int aux = fin_pos;
+    fin_pos = init_pos;
+    init_pos = aux;
+  }
+
+  child.p_size = parent1.p_size;
+
+  // parte to parente 1
+  for(int i = init_pos; i < fin_pos; i++)
+    child.path[i] = parent1.path[i];
+
+  // parte do parente 2
+  for(int i = 0; i < init_pos; i++)
+    child.path[i] = parent2.path[i];
+  for(int i = fin_pos; i < child.p_size; i++)
+    child.path[i] = parent2.path[i];
+}
+
+//------------------------------------------------------------------------------
+// mutate: gera mutacoes na populacao
+//------------------------------------------------------------------------------
+void mutate(individual ind) {
+  double mutation_rate = 0.015; // frequencia da mutacao
+
+  for(int i = 1; i < ind.p_size; i++) {
+    // indice de mutacao
+    if(random_to_one() < mutation_rate) {
+      int pos = rand() % ind.p_size;
+
+      // swap
+      int aux = ind.path[pos];
+      ind.path[pos] = ind.path[i];
+      ind.path[i] = aux;
+    }
+  }
+}
+
+//------------------------------------------------------------------------------
 // GeneticAlgorithm: solucao usando algoritmo genetico
 //------------------------------------------------------------------------------
 double GeneticAlgorithm(MyGraph* g, int* &path) {
+  srand(time(0));
   int size = g->getVertexNum();
   int* tmp_path = generate_random_path(size);
 
