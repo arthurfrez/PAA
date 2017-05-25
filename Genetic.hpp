b5f195f83e9daa2b346ddcdb8af839724779898d
Arthur F. Rezende <arthurfrezende@gmail.com>
2017-05-25 10:53:44 -0300

    Working
    Not optimal, may be doing unnecessary work but is working.

--- Genetic.hpp
+++ Genetic.hpp
@@ -42,7 +42,6 @@ struct individual {
 
   individual();
   individual(individual*);
-  //~individual();
 };
 
 // boo
@@ -52,14 +51,16 @@ void individual::calculate_fitness() {
     fitness += graph->getEdge(path[i], path[i-1]);
   fitness += graph->getEdge(path[size-1], 0);
 }
-individual::individual() {  }
+individual::individual() {
+  size = -1;
+  fitness = -1.0;
+}
 individual::individual(individual * ind) {
   size = ind->size;
   fitness = ind->fitness;
   for(int i = 0; i < size; i++)
     path[i] = ind->path[i];
 }
-//individual::~individual() { delete path; }
 
 //------------------------------------------------------------------------------
 // random_to_one: gera um numero pseudo aleatorio entre 0 e 1
@@ -69,7 +70,7 @@ double random_to_one() { return rand() / (RAND_MAX + 1.0); }
 //------------------------------------------------------------------------------
 // generate_random_path: gera um caminho aleatorio
 //------------------------------------------------------------------------------
-void generate_random_path(int size, int path[MAX_CITY]) {
+void generate_random_path(int size, int (&path)[MAX_CITY]) {
   std::vector<int> tmp(size);
 
   for(int i = 0; i < size; i++) tmp[i] = i; // gerando array sequencial
@@ -113,6 +114,23 @@ int getFittest(individual* pop, int p_size) {
 }
 
 //------------------------------------------------------------------------------
+// getSecond: acha a posicao do segundo individuo mais adaptado
+//------------------------------------------------------------------------------
+int getSecond(individual* pop, int p_size, int fittest_pos) {
+  double fit = 1000000.0;
+  int pos = -1;
+
+  for(int i = 0; i < p_size; i++) {
+    if(pop[i].fitness < fit && i != fittest_pos) {
+      fit = pop[i].fitness;
+      pos = i;
+    }
+  }
+
+  return pos;
+}
+
+//------------------------------------------------------------------------------
 // crossover: mescla os parentes
 //------------------------------------------------------------------------------
 individual crossover(individual parent1, individual parent2) { // ERRADO
@@ -128,17 +146,50 @@ individual crossover(individual parent1, individual parent2) { // ERRADO
 
   child.size = parent1.size;
 
+  bool* hasIt = new bool[child.size];
+  for(int i = 0; i < child.size; i++)
+    hasIt[i] = false;
+
   // parte to parente 1
-  for(int i = init_pos; i < fin_pos; i++)
+  for(int i = init_pos; i < fin_pos; i++) {
     child.path[i] = parent1.path[i];
 
-  // parte do parente 2
-  for(int i = 0; i < init_pos; i++)
-    child.path[i] = parent2.path[i];
-  for(int i = fin_pos; i < child.size; i++)
-    child.path[i] = parent2.path[i];
+    // Ja colocado o vertice no caminho
+    hasIt[parent1.path[i]] = true;
+  }
+
+  // TESTE
+  int a_pos = 0;
+
+  for(int i = 0; i < init_pos; i++) {
+    while(hasIt[parent2.path[a_pos]])
+          a_pos += 1;
+
+    child.path[i] = parent2.path[a_pos++];
+  }
+  for(int i = fin_pos; i < child.size; i++) {
+    while(hasIt[parent2.path[a_pos]])
+          a_pos += 1;
+
+    child.path[i] = parent2.path[a_pos++];
+  }
 
   child.calculate_fitness();
+
+  /*printf("Parent 1: ");
+  for(int i=0;i<parent1.size;i++) printf("%i ", parent1.path[i]);
+  printf("\nParent 2: ");
+  for(int i=0;i<parent2.size;i++) printf("%i ", parent2.path[i]);
+  printf("\nChild: ");
+  for(int i=0;i<child.size;i++) printf("%i ", child.path[i]);
+  printf("\n");*/
+
+  if(child.path[0] != 0) {
+    printf("ERROR: Crossover failed, aborting program.");
+    printf("%i %i %i\n", parent1.path[0], parent2.path[0], child.path[0]);
+    exit (EXIT_FAILURE);
+  }
+
   return child;
 }
 
@@ -151,7 +202,7 @@ void mutate(individual &ind) {
   for(int i = 1; i < ind.size; i++) {
     // indice de mutacao
     if(random_to_one() < mutation_rate) {
-      int pos = rand() % ind.size;
+      int pos = (rand() % (ind.size-1)) + 1;
 
       // swap
       int aux = ind.path[pos];
@@ -161,17 +212,25 @@ void mutate(individual &ind) {
   }
 
   ind.calculate_fitness();
+  if(ind.path[0] != 0) {
+    printf("ERROR: Mutation failed, aborting program.\n");
+    printf("%i\n", ind.path[0]);
+    exit (EXIT_FAILURE);
+  }
 }
 
 //------------------------------------------------------------------------------
-// tournamentSelection: seleciona individual usando selecao por torneio
+// tournamentSelection: seleciona os parentes usando selecao por torneio
 //------------------------------------------------------------------------------
-individual* tournamentSelection(individual* pop, int p_size) {
+void tournamentSelection(individual* pop, int p_size, individual &p1, individual &p2) {
   individual* tournament = new individual[TOURNAMENT_SIZE];
   for(int i = 0; i < TOURNAMENT_SIZE; i++)
-    tournament[i] = pop[rand() % p_size];
+    tournament[i] = pop[(rand() % (p_size-1))+1];
 
-  return &tournament[getFittest(tournament, TOURNAMENT_SIZE)];
+  int fit = getFittest(tournament, TOURNAMENT_SIZE);
+  int sec = getSecond(tournament, TOURNAMENT_SIZE, fit);
+  p1 = individual(tournament[fit]);
+  p2 = individual(tournament[sec]);
 }
 
 //------------------------------------------------------------------------------
@@ -180,16 +239,20 @@ individual* tournamentSelection(individual* pop, int p_size) {
 individual* evolve_pop(individual* oldPop, int size, int p_size) {
   individual* newPop = new individual[p_size];
   newPop[0] = oldPop[getFittest(oldPop, p_size)]; // elitism
-  newPop[1] = individual(newPop[0]);
 
-  for(int i = 2; i < p_size; i++) {
-    individual parent1 = individual(tournamentSelection(oldPop, p_size));
-    individual parent2 = individual(tournamentSelection(oldPop, p_size));
+  for(int i = 1; i < p_size; i++) {
+    individual parent1, parent2;
+    tournamentSelection(oldPop, p_size, parent1, parent2);
+    //individual parent1 = individual(tournamentSelection(oldPop, p_size));
+    //individual parent2 = individual(tournamentSelection(oldPop, p_size));
     individual child = individual(crossover(parent1, parent2));
+    //printf("%0.2f  %0.2f   to: %0.2f\n", parent1.fitness, parent2.fitness, child.fitness);
     newPop[i] = child;
   }
 
-  for(int i = 1; i < p_size; i++) mutate(newPop[i]);
+  for(int i = 1; i < p_size; i++) {
+    mutate(newPop[i]);
+  }
 
   return newPop;
 }
@@ -205,7 +268,7 @@ double GeneticAlgorithm(MyGraph* g, int* &path) {
 
   individual* pop = generate_pop(size, p_size);
   for(int i = 0; i < EVOLUTION_NUM; i++)
-    evolve_pop(pop, size, p_size);
+    pop = evolve_pop(pop, size, p_size);
 
   individual ind = pop[getFittest(pop, p_size)];
   double resp = ind.fitness;
@@ -213,6 +276,6 @@ double GeneticAlgorithm(MyGraph* g, int* &path) {
   path = new int[MAX_CITY];
   for(int i = 0; i < size; i++)path[i] = ind.path[i];
 
-  delete pop;
+  delete [] pop;
   return resp;
 }
