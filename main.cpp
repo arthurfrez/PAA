c240e02ad1da316a5f164ca6fb4ea6c6ad8a087b
Arthur F. Rezende <arthurfrezende@gmail.com>
2017-05-29 10:18:19 -0300

    Execution time added.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "BranchBound.hpp"
 #include "Genetic.hpp"
 #include <cstdio>
+#include <time.h>
 
 //------------------------------------------------------------------------------
 // read_and_construct: metodo para ler entrada e montar o grafo
@@ -35,11 +36,16 @@ int main() {
   int* path = new int[100];
   MyGraph* g = new MyGraph();
   read_and_construct(g);
+  clock_t ex_time = clock(); // tempo do inicio da execucao
 
   //double resp = BruteForce(g, path);
   //double resp = Branch_and_Bound(g, path);
   double resp = GeneticAlgorithm(g, path);
+
+  ex_time = clock() - ex_time; // tempo gasto para executar
+
   print(g, resp, path);
+  printf("\nExecution time: %0.2fs\n", ((float)ex_time)/CLOCKS_PER_SEC);
 
   return 0;
 }
